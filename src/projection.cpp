e1eaa150ab0ae6bee609eb5346e3d4ef859d2ce9
Kohio DEFLESSELLE <kohiodef@gmail.com>
2025-05-07 22:47:16 +0200

    Add transform to projection test

--- src/projection.cpp
+++ src/projection.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-#include <ostream>
 #include <memory>
+#include <ostream>
 
 #include "Eigen/Dense"
-
 #include "yart/file/ppm_writer.h"
-#include "yart/image/canvas.h"
-#include "yart/geometry/sphere.h"
 #include "yart/geometry/intersection.h"
+#include "yart/geometry/sphere.h"
+#include "yart/geometry/transform.h"
+#include "yart/image/canvas.h"
 #include "yart/math/ray.h"
 
 using namespace yart;
 
 int main(int /*argc*/, char* /*argv*/[]) {
-
   image::Canvas<100, 100> canvas;
   image::Color hit_color{1.0, 0, 0};
 
@@ -23,26 +22,25 @@ int main(int /*argc*/, char* /*argv*/[]) {
   float canvas_world_height = 7.0f;
 
   auto sphere = std::make_shared<geometry::Sphere>(1.0f);
+
+  sphere->transform = geometry::Transform3d::Identity() * transform::shear<float>(1, 0, 0, 0, 0, 0)
+                      ;//* transform::scaling<float>(1, 0.5, 1);
+
   double half_width = canvas_world_width / 2;
   double half_height = canvas_world_height / 2;
   double x_pixel_size = canvas_world_width / canvas.getWidth();
   double y_pixel_size = canvas_world_height / canvas.getHeight();
 
-  for (size_t y = 0; y < canvas.getHeight(); y++)
-  {
+  for (size_t y = 0; y < canvas.getHeight(); y++) {
     float world_y = half_height - y * y_pixel_size;
-    for (size_t x = 0; x < canvas.getWidth(); x++)
-    {
+    for (size_t x = 0; x < canvas.getWidth(); x++) {
       float world_x = -half_width + x_pixel_size * x;
       auto ray_origin = Eigen::Vector3f{0, 0, -5};
       auto ray_direction = (Eigen::Vector3f{world_x, world_y, 10} - ray_origin).normalized();
-      Ray r {
-        ray_origin,
-        ray_direction
-      };
+      Ray r{ray_origin, ray_direction};
       auto intersections = sphere->intersections(r);
       auto hit = geometry::hit(intersections);
-      if(hit == nullptr) {
+      if (hit == nullptr) {
         canvas.setPixel(x, canvas.getHeight() - 1 - y, image::BLACK);
         continue;
       }
@@ -52,9 +50,8 @@ int main(int /*argc*/, char* /*argv*/[]) {
     }
   }
 
-  writer.write("/home/sora/projection.ppm", canvas.getPixels(), canvas.getWidth(), canvas.getHeight());
-  
-  
+  writer.write("/home/sora/projection.ppm", canvas.getPixels(), canvas.getWidth(),
+               canvas.getHeight());
 
   return 0;
 }
