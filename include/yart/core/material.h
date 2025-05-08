6cfe39be9a31ff616137b549dd718e84fe26a873
Kohio DEFLESSELLE <kohiodef@gmail.com>
2025-05-08 17:25:31 +0200

    Add Material class

---
+++ include/yart/core/material.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <cstdint>
+
+#include "yart/image/color.h"
+
+namespace yart {
+  class Material {
+  private:
+    color::Color diffuse_color = color::White;
+
+    float ambient = 0.1f;
+    float diffuse = 0.9f;
+    float specular = 0.9f;
+    float shininess = 200.0f;
+
+  public:
+    Material(const color::Color& _diffuse_color, float _ambient, float _diffuse, float _specular,
+             float _shininess);
+    Material(const color::Color& _diffuse_color, float _ambient, float _diffuse, float _specular);
+    Material(const color::Color& _diffuse_color, float _ambient, float _diffuse);
+    Material(const color::Color& _diffuse_color, float _ambient);
+    Material(const color::Color& _diffuse_color);
+    Material() = default;
+    ~Material();
+
+    float get_ambient() const;
+    float get_diffuse() const;
+    float get_specular() const;
+    float get_shininess() const;
+    color::Color get_diffuse_color() const;
+
+    Material& set_ambient(float _ambient);
+    Material& set_diffuse(float _diffuse);
+    Material& set_specular(float _specular);
+    Material& set_shininess(float _shininess);
+    Material& set_diffuse_color(const color::Color& _diffuse_color);
+    Material& set_diffuse_color(float r, float g, float b);
+    Material& set_diffuse_color(float r, float g, float b, float a);
+  };
+}  // namespace yart
---
+++ src/core/material.cpp
@@ -0,0 +1,41 @@
+#include "yart/core/material.h"
+
+namespace yart {
+  Material::~Material() {}
+
+  float Material::get_ambient() const { return ambient; }
+  float Material::get_diffuse() const { return diffuse; }
+  float Material::get_specular() const { return specular; }
+  float Material::get_shininess() const { return shininess; }
+  color::Color Material::get_diffuse_color() const { return diffuse_color; }
+
+  Material& Material::set_ambient(float _ambient) {
+    ambient = _ambient;
+    return (*this);
+  }
+  Material& Material::set_diffuse(float _diffuse) {
+    diffuse = _diffuse;
+    return (*this);
+  }
+  Material& Material::set_specular(float _specular) {
+    specular = _specular;
+    return (*this);
+  }
+  Material& Material::set_shininess(float _shininess) {
+    shininess = _shininess;
+    return (*this);
+  }
+  Material& Material::set_diffuse_color(const color::Color& _diffuse_color) {
+    diffuse_color = _diffuse_color;
+    return (*this);
+  }
+  Material& Material::set_diffuse_color(float r, float g, float b) {
+    diffuse_color = color::Color(r, g, b);
+    return (*this);
+  }
+  Material& Material::set_diffuse_color(float r, float g, float b, float a) {
+    diffuse_color = color::Color(r, g, b, a);
+    return (*this);
+  }
+
+}  // namespace yart
