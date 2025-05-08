b99f83c2bb0e8cafe28c91126a93758519938263
Kohio DEFLESSELLE <kohiodef@gmail.com>
2025-05-08 17:24:18 +0200

    Add Vector3 reflect to math module

--- src/math/math.cpp
+++ src/math/math.cpp
@@ -4,5 +4,13 @@
 #include <type_traits>
 
 namespace yart {
-  namespace math {}  // namespace math
+
+  namespace math {
+
+    Eigen::Vector3f reflect(const Eigen::Vector3f& vector, const Eigen::Vector3f& normal) {
+      return vector - normal * 2 * vector.dot(normal);
+    }
+
+  }  // namespace math
+  
 }  // namespace yart
