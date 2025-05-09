c3f3697d6d9796f8ec21a39683827baa6e505041
Kohio DEFLESSELLE <kohiodef@gmail.com>
2025-05-09 13:26:03 +0200

    Add sphere transforming to shaded_sphere example

--- src/shaded_sphere.cpp
+++ src/shaded_sphere.cpp
@@ -22,9 +22,14 @@ int main(int /*argc*/, char* /*argv*/[]) {
   float canvas_world_height = 7.0f;
 
   Material material;
-  material.set_diffuse_color(0.3, 0.25, 1);
+  material.set_diffuse_color(0.2, 0.4, 1);
 
-  auto sphere = std::make_shared<geometry::Sphere>(1.0f);
+  geometry::Transform3D transform = geometry::Transform3D::Identity();
+  transform = transform * transform::shear<float>(1, 0, 0, 0, 0, 0);
+  transform.rotate(Eigen::AngleAxisf(M_PI / 6, Eigen::Vector3f::UnitY()))
+  .rotate(Eigen::AngleAxisf(M_PI / 6, Eigen::Vector3f::UnitX()));
+  
+  auto sphere = std::make_shared<geometry::Sphere>(transform, 1.0f);
   sphere->set_material(material);
 
   auto light = light::PointLight{Eigen::Vector3f{-10, 10, -10}};
@@ -46,7 +51,7 @@ int main(int /*argc*/, char* /*argv*/[]) {
       auto ray_direction = (Eigen::Vector3f{world_x, world_y, 10} - ray_origin).normalized();
 
       Ray r{ray_origin, ray_direction};
-      
+
       auto intersections = sphere->intersections(r);
       auto hit = geometry::hit(intersections);
 
@@ -65,8 +70,7 @@ int main(int /*argc*/, char* /*argv*/[]) {
     }
   }
 
-  writer.write("/home/sora/shaded.ppm", canvas.getPixels(), canvas.getWidth(),
-               canvas.getHeight());
+  writer.write("/home/sora/shaded.ppm", canvas.getPixels(), canvas.getWidth(), canvas.getHeight());
 
   return 0;
 }
